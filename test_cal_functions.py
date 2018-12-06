f52d798c3606cf57005a7839c9838457c6532fb2
h3xdrgN <rohanshirodkar29@gmail.com>
2018-12-05 19:11:08 -0500

    added new tests for subtract, multiply, divide with same assertEqual. added new assertRaise to check for value error condition in divide function

--- test_cal_functions.py
+++ test_cal_functions.py
@@ -14,6 +14,27 @@ class TestCalFunctions(unittest.TestCase):
         self.assertEqual(cal_functions.add(-1, -1), -2)
         self.assertEqual(cal_functions.add(-5, 7), 2)
 
+    def test_subtract(self):
+        """add function test"""
+        self.assertEqual(cal_functions.subtract(10, 5), 5)
+        self.assertEqual(cal_functions.subtract(-1, -1), 0)
+        self.assertEqual(cal_functions.subtract(-5, 7), -12)
+
+    def test_multiply(self):
+        """add function test"""
+        self.assertEqual(cal_functions.multiply(10, 5), 50)
+        self.assertEqual(cal_functions.multiply(-1, -1), 1)
+        self.assertEqual(cal_functions.multiply(-5, 7), -35)
+
+    def test_divide(self):
+        """add function test"""
+        self.assertEqual(cal_functions.divide(10, 5), 2)
+        self.assertEqual(cal_functions.divide(-1, -1), 1)
+        self.assertEqual(cal_functions.divide(-5, -5), 1)
+
+        with self.assertRaises(ValueError):
+            cal_functions.divide(5, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
